863387ce269b6348ed2722d2265fd4c39f9713c2
Ayesha Khan <ayeshakay786@gmail.com>
2025-02-04 18:26:57 -0800

    postMessage for checking error on why names are displaying

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 figma.showUI(__html__, { width: 1000, height: 600 });
 
-// Fetch the top-level pages from the Figma file (figma.root.children)
+// Fetch the top-level pages from the Figma file
 const pages = figma.root.children;
 console.log('Fetched Pages:', pages);
 
@@ -8,8 +8,18 @@ console.log('Fetched Pages:', pages);
 const pageNames = pages.map(page => page.name);
 console.log('Page Names:', pageNames);  // Log the names of the pages
 
+// Check if pageNames is correctly populated
+if (pageNames.length > 0) {
+    console.log('Pages are available:', pageNames);
+} else {
+    console.error('No pages found in the Figma file.');
+}
+
 // Send the page names to the UI to display in the checklist
-figma.ui.postMessage({ type: 'displayPages', pages: pageNames });
+figma.ui.postMessage({
+    type: 'displayPages',
+    pages: pageNames // Send the array of page names to the UI
+});
 
 // Handle when the UI sends a message about selected pages
 figma.ui.onmessage = (msg) => {
